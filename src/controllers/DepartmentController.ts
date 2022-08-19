96f5c72cf2eb081484cb500228213960454685b8
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-08-19 22:09:43 +0700

    add validation for the request parameters: page & limit

--- src/controllers/DepartmentController.ts
+++ src/controllers/DepartmentController.ts
@@ -6,16 +6,28 @@ const DepartmentController = (app: Express) => {
   const service = new DepartmentService();
 
   app.get("/departments", async (req: Request, res: Response, next: NextFunction) => {
-    
-    const params: Params = req.query;
-    const page = params["page"] || 1;
-    const limit = params["limit"] || 5;
-    
-    const data: Department[] = service.getDepartments(page, limit);
-
-    return res.status(200).json({
-      data
-    })
+    try {
+      const params: Params = req.query;
+      const page = params["page"] || 1;
+      const limit = params["limit"] || 5;
+      
+      const totalItems: number = service.getTotalDepartments();
+      const totalPages: number = Math.ceil(totalItems / limit);
+      
+      if (page > totalPages)
+        throw new Error(`Page not found. Page ${page} is requested, only ${totalPages} pages are available.`);
+      
+      const data: Department[] = service.getDepartments(page, limit);
+  
+      return res.status(200).json({
+        data
+      })
+    }
+    catch (e: any) {
+      return res.status(500).json({
+        error: e.message
+      })
+    }
   });
 }
 
--- src/services/DepartmentService.ts
+++ src/services/DepartmentService.ts
@@ -2,10 +2,14 @@ import departments from "../data/departments.json";
 import { Department } from "../types";
 
 class DepartmentService {
+
+  public getTotalDepartments(): number {
+    return departments.data.length;
+  }
+
   public getDepartments(page: number, limit: number): Department[] {
-    
     const startIndex: number = (page - 1) * limit;
-    const endIndex: number = startIndex + limit;
+    const endIndex: number = startIndex + +limit;
     const data = departments.data.slice(startIndex, endIndex);
 
     return data
